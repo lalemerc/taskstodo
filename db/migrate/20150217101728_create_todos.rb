974fcddc544c191f9fdd987b280dc60d582d6847
laure lemercinier <laure.lemercinier@gmail.com>
2015-02-17 11:25:06 +0100

    new todo model

---
+++ db/migrate/20150217101728_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration
+  def change
+    create_table :todos do |t|
+      t.string :name
+      t.text :content
+      t.datetime :created
+
+      t.timestamps null: false
+    end
+  end
+end
